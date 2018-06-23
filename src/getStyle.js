cd30e9fcf189b0e2077f2c27fbeb523aac6ad3a3
Haz <hazdiego@gmail.com>
2018-06-23 18:09:13 -0300

    getStyle (#44)

---
+++ src/getStyle.js
@@ -0,0 +1,43 @@
+// @flow
+type InterpolatedFn = (props: Object) => any;
+type Part = string | InterpolatedFn | Part[];
+
+const mapRules = (props: Object) => (part: Part) => {
+  if (typeof part === "function") {
+    return mapRules(props)(part(props));
+  }
+  if (Array.isArray(part)) {
+    return part.map(mapRules(props)).join("");
+  }
+  return part;
+};
+
+/**
+ * Get the style rules from a given styled component.
+ *
+ * @example
+ * import styled from "styled-components";
+ * import { getStyle, prop } from "styled-tools";
+ *
+ * const Background = styled.div`
+ *   display: block;
+ *   background-color: ${prop("bg")};
+ * `;
+ *
+ * const BlackBackground = styled.div`
+ *   ${getStyle(Background, { bg: "black" })};
+ * `;
+ */
+const getStyle = (
+  styledComponent: Object = {},
+  props: Object = styledComponent.defaultProps || {}
+) => {
+  if (!styledComponent.componentStyle) {
+    throw new Error(
+      "[styled-tools] getStyle expects to receive a styled component."
+    );
+  }
+  return styledComponent.componentStyle.rules.map(mapRules(props)).join("");
+};
+
+export default getStyle;
