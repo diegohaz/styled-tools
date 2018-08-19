20859ceb049472fd6951df6cb6a8dac19035d021
Haz <hazdiego@gmail.com>
2018-08-19 05:05:14 -0300

    Remove second argument from Needle

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-type Needle<Props, T> = string | ((props?: Props) => T);
+type Needle<Props> = string | ((props?: Props) => any);
 
 export function prop<T = undefined>(
   path: string,
@@ -11,24 +11,24 @@ export function theme<T = undefined>(
 ): <Props, Theme>(props: Props & { theme: Theme }) => Theme[keyof Theme] | T;
 
 export function ifProp<Props, Pass = undefined, Fail = undefined>(
-  test: Needle<Props, any> | Needle<Props, any>[] | { [key: string]: any },
+  test: Needle<Props> | Needle<Props>[] | { [key: string]: any },
   pass?: Pass,
   fail?: Fail
 ): <P = Props>(props?: P) => Pass | Fail;
 
 export function ifNotProp<Props, Pass = undefined, Fail = undefined>(
-  test: Needle<Props, any> | Needle<Props, any>[] | { [key: string]: any },
+  test: Needle<Props> | Needle<Props>[] | { [key: string]: any },
   pass?: Pass,
   fail?: Fail
 ): <P = Props>(props?: P) => Pass | Fail;
 
 export function switchProp<Props, T = undefined, DefaultCase = undefined>(
-  needle: Needle<Props, any>,
+  needle: Needle<Props>,
   cases: { [key: string]: T },
   defaultCase?: DefaultCase
 ): <P = Props>(props?: P) => T | DefaultCase;
 
 export function withProp<Props, T = undefined>(
-  needle: Needle<Props, any> | Needle<Props, any>[],
+  needle: Needle<Props> | Needle<Props>[],
   fn: (...args: any[]) => T
 ): <P = Props>(props?: P) => T;
